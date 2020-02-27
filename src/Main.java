64ab76d32591c245e31559e917b9f429281f3d49
nahte2000 <ethandith@gmail.com>
2020-02-26 18:07:14 -0800

    Locally Fixed

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,13 @@ public class Main {
 
     @Test
     public void testBubbleSort() {
-
-        int[] inputs = {5,4,3,2,1};
-        int[] expects = {1,2,3,4,5};
-
+        int[] inputs = {5, 4, 3, 2, 1};
+        int[] expects = {1, 2, 3, 4, 5};
         bubbleSort(inputs);
-
         assertTrue(Arrays.equals(inputs, expects));
     }
 
-	// to sort in ascending order
     public static void bubbleSort(int[] data) {
-		// check data size
-		if (data.length>0) {
-			return;
-		}
-
-		// sort
         for (int i=0; i<data.length; i++) {
             for (int j=0; j<data.length-i-1; j++) {
                 if (data[j]>data[j+1]) {
